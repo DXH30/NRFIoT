e2a45c3f1e83e57e84fa60c05ce94a0b17ea663c
DXH30 <dhs0223@gmail.com>
2019-04-25 00:30:58 +0800

    Perkembangan Transmitter

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,41 +1,86 @@
-#include "nRF24L01.h" //NRF24L01 library created by TMRh20 https://github.com/TMRh20/RF24
+#include "nRF24L01.h"
 #include "RF24.h"
 #include "SPI.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
 
-#define SwitchPin 8 // Arcade switch is connected to Pin 8 on NANO
-int SentMessage[1] = {000}; // Used to store value before being sent through the NRF24L01
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+MPU6050 accelgyro(0x68);
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
 
-RF24 radio(9,10); // NRF24L01 used SPI pins + Pin 9 and 10 on the NANO
+#define OUTPUT_READABLE_ACCELGYRO
 
-const uint64_t pipe = 0xE6E6E6E6E6E6; // Needs to be the same for communicating between 2 NRF24L01 
+#define LED_PIN 13
+bool blinkState = false;
 
-void setup(void){
+#define SwitchPin 8
+int SentMessage[1] = {000};
 
-pinMode(SwitchPin, INPUT_PULLUP); // Define the arcade switch NANO pin as an Input using Internal Pullups
-digitalWrite(SwitchPin,HIGH); // Set Pin to HIGH at beginning
+RF24 radio(9, 10);
 
-radio.begin(); // Start the NRF24L01
-radio.openWritingPipe(pipe); // Get NRF24L01 ready to transmit
-Serial.begin(9600);
+const uint64_t pipe = 0xE6E6E6E6E6E6; // Alamat NRF
+
+const unsigned int TRIG_PIN = 5;
+const unsigned int ECHO_PIN = 6;
+
+void setup(void) {
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
+  pinMode(SwitchPin, INPUT_PULLUP);
+  digitalWrite(SwitchPin, HIGH);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  radio.begin();
+  radio.openWritingPipe(pipe);
+  Serial.println("Initializing I2C devices...");
+  accelgyro.initialize();
+  Serial.begin(38400);
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 }
 int a = 0;
 
-void loop(void){
+void loop() {
+  /*
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+  */
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-if (digitalRead(SwitchPin) == LOW){ // If Switch is Activated
-SentMessage[0] = 111;
-radio.write(SentMessage, 1); // Send value through NRF24L01
-if(a == 1) {
-  Serial.println("Switch is Activated");
-  a = 0;
-}
-}
-else {
-SentMessage[0] = 000;
-radio.write(SentMessage, 1);
-if(a == 0) {
-  Serial.println("Switch is not Activated");
-  a = 1;
-}
-}
+ // const unsigned long duration = pulseIn(ECHO_PIN, HIGH);
+//  int distance = duration / 29 / 2;
+  #ifdef OUTPUT_READABLE_ACCELGYRO
+    // display tab-separated accel/gyro x/y/z values
+    Serial.print("a/g:\t");
+    Serial.print(ax); Serial.print("\t");
+    Serial.print(ay); Serial.print("\t");
+    Serial.print(az); Serial.print("\t");
+    Serial.print(gx); Serial.print("\t");
+    Serial.print(gy); Serial.print("\t");
+    Serial.print(gz); Serial.print("\n");
+//    Serial.println(distance);
+  #endif
+
+  #ifdef OUTPUT_BINARY_ACCELGYRO
+    Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
+    Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
+    Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
+    Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
+    Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
+    Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
+  #endif
+   // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
 }
